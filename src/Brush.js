696f0ba8721e73cce4e8de86ab33d805d60f9765
Eli Crow <elimichaelcrow@gmail.com>
2017-02-21 20:27:26 -0500

    Refactored brush for hygiene

--- src/Brush.js
+++ src/Brush.js
@@ -1,49 +1,30 @@
 const SmoothPointer = require('./SmoothPointer.js');
 const canvasutils = require('./canvasutils.js');
 
-const SPEED_SCALE = 200;
-var sin = Math.sin,
-    cos = Math.cos,
-    abs = Math.abs,
-    PI = Math.PI,
-    max = Math.max,
-    min = Math.min,
-    sqrt = Math.sqrt,
-    pow = Math.pow,
-    atan2 = Math.atan2,
-    random = Math.random,
-    sign = Math.sign;
-
 class Brush 
 {
   constructor(canvas) {
     this._ctx = canvas.getContext('2d');
+    this._tex = new Image();
+    this._tempCanvas = document.createElement('canvas');
+    this._tempCtx = this._tempCanvas.getContext('2d');
 
-    this.pressureSensitivity = 0;
-    this.speedSensitivity = 0;
-    this.angleSensitivity = 15;
     this.minSize = 2;
-    this.calligAngle = 135;
     this.erase = false;
-    this.maxSize = 20;
-    this.hasSymmetricalEmphasis = false;
     this.alpha = 255;
-    
-    //offscreen canvas to maniupulate and read brush image data.
-    this.tempCanvas = document.createElement('canvas');
-    this.tempCtx = this.tempCanvas.getContext('2d');
+    this.pressureSensitivity = 0;
+    this.angleSensitivity = 15;
+    this.speedSensitivity = 0;
+    this.speedScale = 200;
+    this.calligAngle = 135;
+    this.hasSymmetricalEmphasis = false;
 
     this.setBrushImage(document.getElementById('brush-shape-bristles'));
-
-    // tex is the result of manipulation
-    this._tex = new Image();
-    this._tex.width = this.brushImg.width;
-    this._tex.height = this.brushImg.height;
-
     this.setBrushColor([127,127,127,this.alpha]);
+
     this.pointer = new SmoothPointer(canvas, {
       minDistance: 2,
-      steps: 20,
+      steps: 15,
       down: e => {
         if (this.erase) _ctx.globalCompositeOperation = 'destination-out';
         this._drawBrush(e, [e.offsetX, e.offsetY]);
@@ -55,39 +36,39 @@ class Brush
 
   setBrushImage (brushImg) {
     this.brushImg = brushImg;
-    this.tempCanvas.width = brushImg.width;
-    this.tempCanvas.height = brushImg.height;
+    this._tex.width = brushImg.width;
+    this._tex.height = brushImg.height;
+    this._tempCanvas.width = brushImg.width;
+    this._tempCanvas.height = brushImg.height;
   }
 
-  //render the brush offscreen so we can manipulate the color/data, store in this._tex
   //color array is rgba, all in the range of 0-255;
   setBrushColor (colorArray) {
-    const img = this.brushImg;
-    const tempCanvas = this.tempCanvas;
-    const ctx = this.tempCtx;
-
-    ctx.drawImage(img, 0,0);
-    const image = ctx.getImageData(0,0,img.width,img.height);
-    for (let i = 0, ii = image.data.length; i < ii; i+=4) {
-      image.data[i + 0] = colorArray[0];
-      image.data[i + 1] = colorArray[1];
-      image.data[i + 2] = colorArray[2];
-      image.data[i + 3] *= colorArray[3] / 255;
+    const inBrushImg = this.brushImg;
+    const ctx = this._tempCtx;
+    ctx.drawImage(inBrushImg, 0,0);
+    
+    const outBrushImg = ctx.getImageData(0,0,inBrushImg.width,inBrushImg.height);
+    for (let i = 0, ii = outBrushImg.data.length; i < ii; i+=4) {
+      outBrushImg.data[i + 0] = colorArray[0];
+      outBrushImg.data[i + 1] = colorArray[1];
+      outBrushImg.data[i + 2] = colorArray[2];
+      outBrushImg.data[i + 3] *= colorArray[3] / 255;
     }
 
-    ctx.putImageData(image,0,0);
-    this._tex.src = tempCanvas.toDataURL('image/png');
+    ctx.putImageData(outBrushImg,0,0);
+    this._tex.src = this._tempCanvas.toDataURL('image/png');
   }
   
   _drawBrush (e, pts) {
     var s = Math.sqrt(e.squaredSpeed);
     var symm = this.hasSymmetricalEmphasis ? 2 : 1;
     
-    var size = this.minSize + max( 0,
-        this.speedSensitivity    * s/(s+SPEED_SCALE)
+    var size = this.minSize + Math.max( 0,
+        this.speedSensitivity    * s/(s+this.speedScale)
       + this.pressureSensitivity * e.pressure
       + this.angleSensitivity    * (
-          cos((Math.PI - e.direction + this.calligAngle)*symm) *.5 +.5
+          Math.cos((Math.PI - e.direction + this.calligAngle)*symm) *.5 +.5
         )
     );
 
