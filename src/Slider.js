ac98675c50962edcce169244dbf7ca42a1d7b72e
Eli Crow <elimichaelcrow@gmail.com>
2017-03-01 17:53:10 -0500

    signed slider center indicators now show behind thumb in webkit

--- src/Slider.js
+++ src/Slider.js
@@ -5,7 +5,6 @@ module.exports = class Slider extends InputBase
   constructor (value, min, max, step, name) {
     super('range', { name: name, value: value, min: min, max: max, step: step });
     this._element.classList.add('iris-range');
-    if (min < 0) 
-    	this._input.classList.add('signed');
+    if (min < 0) this._element.classList.add('signed');
   }
 }
